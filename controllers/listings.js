ec5f1460f0f1603bf15df4063c4b7fe293c63d36
Ujjwal Kumar23 <ujjwaljwala369@gmail.com>
2026-03-14 15:29:26 +0530

    fix render deploy

--- controllers/listings.js
+++ controllers/listings.js
@@ -32,26 +32,35 @@ module.exports.showListing = async (req, res) => {
 };
 
 module.exports.createListing = async (req, res, next) => {
-  let response = await geocodingClient
-    .forwardGeocode({
-      query: req.body.listing.location,
-      limit: 1,
-    })
-    .send();
+  try {
+    let response = await geocodingClient
+      .forwardGeocode({
+        query: req.body.listing.location,
+        limit: 1,
+      })
+      .send();
 
-  let url = req.file.path;
-  let filename = req.file.filename;
+    let url = "";
+    let filename = "";
 
-  const newListing = new Listing(req.body.listing);
-  newListing.owner = req.user._id;
-  newListing.image = { url, filename };
+    if (req.file) {
+      url = req.file.path;
+      filename = req.file.filename;
+    }
 
-  newListing.geometry =  response.body.features[0].geometry;
+    const newListing = new Listing(req.body.listing);
+    newListing.owner = req.user._id;
+    newListing.image = { url, filename };
 
-  let savedListing = await newListing.save();
-  console.log(savedListing);
-  req.flash("success", "New Listing Created!");
-  res.redirect("/listings");
+    newListing.geometry = response.body.features[0].geometry;
+
+    await newListing.save();
+
+    req.flash("success", "New Listing Created!");
+    res.redirect("/listings");
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports.renderEditForm = async (req, res) => {
